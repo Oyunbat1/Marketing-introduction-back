12aae19febd8fa575f8e4d5ad43a8d2caa4b7afb
Oyunbat <oyunbat9958@gmail.com>
2025-12-03 13:10:24 +0800

    schema model changed

--- src/model/Message.ts
+++ src/model/Message.ts
@@ -1,19 +1,23 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-export interface IMessage extends Document{
-    name:string;
-    email:string;
-    social:string;
-    company:string;
-    service:string;
-    message:string
+export interface IMessage extends Document {
+    name: string;
+    email: string;
+    phoneNumber?: string;  // Changed from optional based on common practice. Use string! if mandatory.
+    companyName: string;   // Updated from 'social'
+    role: string;          // Updated from 'company'
+    service: string;
+    message: string
 }
+
 const MessageSchema = new Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true},
-    social:{type:String,required:true},
-    company:{type:String,required:true},
-    service:{type:String,required:true},
-    message:{type:String,required:true}
-},{timestamps:true})
-export default  mongoose.model<IMessage>("Message",MessageSchema)
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: String, required: false }, // Updated field, set to 'false' if optional
+    companyName: { type: String, required: true }, // Updated from 'social'
+    role: { type: String, required: true },        // Updated from 'company'
+    service: { type: String, required: true },
+    message: { type: String, required: true }
+}, { timestamps: true })
+
+export default mongoose.model<IMessage>("Message", MessageSchema);
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -4,26 +4,31 @@ const typeDefs = gql`
   type Message {
     id: ID!
     name: String!
-    social: String!
     email: String!
-    company: String!
+    phoneNumber: String    # New Field, nullable (optional)
+    companyName: String!   # Updated from 'social'
+    role: String!          # Updated from 'company'
     service: String!
     message: String!
     createdAt: String!
     updatedAt: String!
   }
+
   type Query {
     getMessages: [Message!]!
   }
-    type Mutation {
+
+  type Mutation {
     createMessage(
       name: String!
       email: String!
-      social: String!
-      company: String!
+      phoneNumber: String  # New Field, nullable (optional)
+      companyName: String! # Updated from 'social'
+      role: String!        # Updated from 'company'
       service: String!
       message: String!
     ): Message!
   }
 `;
+
 export default typeDefs;
